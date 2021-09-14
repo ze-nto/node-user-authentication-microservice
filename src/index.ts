e8074a95bd091c3ae95dff9098bb3f913dba46de
Jose Pereira <josepsneto@gmail.com>
2021-09-14 17:12:28 -0300

    Implements CRUD operations endpoints for users

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import express, {Request, Response, NextFunction} from 'express';
+import usersRoute from './routes/users.route';
+
 const app = express();
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+//Configurações da Aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-    res.status(200).send({
-        foo: 'bar'
-    });
+//Rotas
+app.use(usersRoute);
 
+app.get('/status', (req: Request, res: Response, next: NextFunction) => { 
+    res.status(200).send({foo: 'bar'});
 });
 
+//Inicalização do Servidor
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000.')
 })
